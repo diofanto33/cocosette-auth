806706a3565f8ee31a514a70ef4b1e336501809c
diofanto33 <diego.rodriguez.ramirez@mi.unc.edu.ar>
2024-04-05 01:54:23 -0300

    Defined database port

---
+++ internal/ports/db.go
@@ -0,0 +1,12 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/diofanto33/cocosette-auth/internal/application/core/domain"
+)
+
+type DBPort interface {
+	Get(ctx context.Context, email string) (domain.User, error)
+	Save(context.Context, *domain.User) error
+}
